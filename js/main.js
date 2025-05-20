879e54138675269b222dad78ecb27a051203a2bc
maiferna <maiderfernandez57@gmail.com>
2025-05-20 11:45:14 +0200

    ADD: html, css y estructura js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+/**
+ * Llamar a los elementos del DOM
+ */
+
+const bodyTabla = document.querySelector('#bodyTabla');
+const formulario = document.querySelector('#formulario');
+
+/**
+ * Crear array productos
+ */
+
+
+let arrayProductos = [
+    // {
+    //     id: 'producto-1',
+    //     producto: 'producto 1',
+    //     cantidad: 1,
+    // }
+];
+
+
+/**
+ * Eventos
+ * Agregar y eliminar producto
+ */
+
+
+const agregarProducto()
+
+const eliminarProducto()
+
